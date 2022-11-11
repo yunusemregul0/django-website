b940665ef8a05977917cb935e1f37ecb39dafc37
“sadikturan” <sadikturan@gmail.com>
2022-11-11 15:26:51 +0300

    284_2_4_uygulama_urls

--- courses/urls.py
+++ courses/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 from . import views
 
-# http://127.0.0.1:8000/client        => anasayfa
-# http://127.0.0.1:8000/client/home    => anasayfa
-# http://127.0.0.1:8000/client/kurslar => kurs listesi
-
 urlpatterns = [
-    path('', views.home),
-    path('anasayfa', views.home),
-    path('kurslar', views.kurslar),
+    path('', views.kurslar),
+    path('list', views.kurslar),
+    path('details', views.details),
+    path('programlama', views.programlama),
+    path('mobil-uygulamalar', views.mobiluygulamalar)
 ]
--- courses/views.py
+++ courses/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-    return HttpResponse('anasayfa')
 
 def kurslar(request):
     return HttpResponse('kurs listesi')
+
+def details(request):
+    return HttpResponse('kurs detay sayfası')
+
+def programlama(request):
+    return HttpResponse('programlama kurs listesi')
+
+def mobiluygulamalar(request):
+    return HttpResponse('mobil uygulamalar kurs listesi')
