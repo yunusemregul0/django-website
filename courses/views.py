0e761107c5e1ac021233191d9b3828186a17adff
“sadikturan” <sadikturan@gmail.com>
2022-11-17 22:48:34 +0300

    282_3_9_verilerin_eklenmesi

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,4 @@
+from datetime import date
 from django.shortcuts import redirect, render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
@@ -8,20 +9,42 @@ data = {
     "mobil":"mobil kategorisine ait kurslar",
 }
 
-def index(request):
-    return render(request, 'courses/index.html')
+db = {
+    "courses": [
+        {
+            "title":"javascript kursu",
+            "description":"javascript kurs açıklaması",
+            "imageUrl": "https://img-c.udemycdn.com/course/750x422/1662526_fc1c_3.jpg",
+            "slug": "javascript-kursu",
+            "date": date(2022,10,10),
+            "is-active": True
+        },
+        {
+            "title":"python kursu",
+            "description":"python kurs açıklaması",
+            "imageUrl": "https://img-c.udemycdn.com/course/750x422/2463492_8344_3.jpg",
+            "slug": "python-kursu",
+            "date": date(2022,9,10),
+            "is-active": False
+        },
+        {
+            "title":"web geliştirme kursu",
+            "description":"web geliştirme kurs açıklaması",
+            "imageUrl": "https://img-c.udemycdn.com/course/750x422/1258436_2dc3_4.jpg",
+            "slug": "web-gelistirme-kursu",
+            "date": date(2022,8,10),
+            "is-active": True
+        }
+    ],
+    "categories": ["programlama","web geliştirme","mobil uygulamalar"]
+}
 
-def kurslar(request):
-    list_items = ""
+def index(request):
     category_list = list(data.keys())
 
-    for category in category_list:
-        redirect_url = reverse('courses_by_category', args=[category])
-        list_items += f"<li><a href='{redirect_url}'>{category}</a></li>"
-
-    html = f"<h1>kurs listesi</h1><br><ul>{list_items}</ul>"
-
-    return HttpResponse(html)
+    return render(request, 'courses/index.html', {
+        'categories': category_list
+    })
 
 def details(request, kurs_adi):
     return HttpResponse(f"{kurs_adi} detay sayfası")
