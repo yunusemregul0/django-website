d2b6d21bd84263a70c2fb5f4c882506e6c97195a
“sadikturan” <sadikturan@gmail.com>
2022-11-25 16:30:01 +0300

    284_5_2_modelin_panele_eklenmesi

--- courses/models.py
+++ courses/models.py
@@ -7,16 +7,19 @@ class Course(models.Model):
     imageUrl = models.CharField(max_length=50, blank=False)
     date= models.DateField()
     isActive = models.BooleanField()
-    slug = models.SlugField(default="",null=False, unique=True, db_index=True) 
+    slug = models.SlugField(default="",blank=True,editable=False, null=False, unique=True, db_index=True) 
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
         super().save(args,kwargs)
 
     def __str__(self):
-        return f"{self.title} {self.date}"
+        return f"{self.title}"
 
 
 class Category(models.Model):
     name = models.CharField(max_length=40)
     slug = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.name}"
