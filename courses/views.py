a77ab20f16796c0cd2bbbe8fead9df9ee4c4c55f
“sadikturan” <sadikturan@gmail.com>
2022-11-25 11:33:32 +0300

    284_4_12_slugfield_ile_sorgulama

--- courses/views.py
+++ courses/views.py
@@ -56,13 +56,8 @@ def index(request):
         'courses': kurslar
     })
 
-def details(request, kurs_id):
-    # try: 
-    #     course = Course.objects.get(pk=kurs_id)
-    # except:
-    #     raise Http404()
-
-    course = get_object_or_404(Course, pk=kurs_id)
+def details(request, slug):
+    course = get_object_or_404(Course, slug=slug)
 
     context = {
         'course': course
