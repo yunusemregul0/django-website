49036055ab60521d727f3993be23323dd4b40ec0
“sadikturan” <sadikturan@gmail.com>
2022-11-25 10:36:05 +0300

    284_4_11_slug_field

--- courses/models.py
+++ courses/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 class Course(models.Model):
     title = models.CharField(max_length=50)
@@ -6,6 +7,11 @@ class Course(models.Model):
     imageUrl = models.CharField(max_length=50, blank=False)
     date= models.DateField()
     isActive = models.BooleanField()
+    slug = models.SlugField(default="",null=False) 
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(args,kwargs)
 
     def __str__(self):
         return f"{self.title} {self.date}"
