cef58fce86347a192d87c447e481cf58e4c14e14
“sadikturan” <sadikturan@gmail.com>
2022-12-02 13:19:10 +0300

    284_7_6_http_post_bilgilerin_alinmasi

--- courses/views.py
+++ courses/views.py
@@ -13,7 +13,25 @@ def index(request):
     })
 
 def create_course(request):
+    if request.method == "POST":
+        title = request.POST["title"]
+        description = request.POST["description"]
+        imageUrl = request.POST["imageUrl"]
+        slug = request.POST["slug"]
+        isActive = request.POST.get("isActive", False)
+        isHome = request.POST.get("isHome", False)
+
+        if isActive == "on":
+            isActive = True
+
+        if isHome == "on":
+            isHome = True
+
+        kurs = Course(title=title, description = description,imageUrl=imageUrl, slug = slug, isActive = isActive, isHome = isHome)
+        kurs.save()
+        return redirect("/kurslar")
     return render(request, "courses/create-course.html")
+
 
 def search(request):
     if "q" in request.GET and request.GET["q"] != "":
