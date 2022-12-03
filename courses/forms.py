f33fabc5c2839336c66d1159f7a2d54cc29890da
“sadikturan” <sadikturan@gmail.com>
2022-12-03 20:14:31 +0300

    284_7_9_posting_form_class

--- courses/forms.py
+++ courses/forms.py
@@ -4,4 +4,4 @@ class CourseCreateForm(forms.Form):
     title = forms.CharField()
     description = forms.CharField(widget=forms.Textarea)
     imageUrl = forms.CharField()
-    slug = forms.CharField()
+    slug = forms.SlugField()
--- courses/views.py
+++ courses/views.py
@@ -13,6 +13,18 @@ def index(request):
     })
 
 def create_course(request):
+    if request.method == "POST":
+        form = CourseCreateForm(request.POST)
+
+        if form.is_valid():
+            kurs = Course(
+                title=form.cleaned_data["title"],
+                description=form.cleaned_data["description"],
+                imageUrl=form.cleaned_data["imageUrl"],
+                slug = form.cleaned_data["slug"])
+            kurs.save()
+            return redirect("/kurslar")
+
     form = CourseCreateForm()
     return render(request, "courses/create-course.html", {"form":form})
 
