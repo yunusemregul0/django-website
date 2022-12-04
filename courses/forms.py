8377604391ff958ef7da047b1583971331e3a7ad
“sadikturan” <sadikturan@gmail.com>
2022-12-04 20:37:58 +0300

    284_7_11_form_properties

--- courses/forms.py
+++ courses/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 
 class CourseCreateForm(forms.Form):
-    title = forms.CharField()
+    title = forms.CharField(label="kurs başlığı",required=True, error_messages={
+        "required":"kurs başlığı girmelisiniz."
+    })
     description = forms.CharField(widget=forms.Textarea)
     imageUrl = forms.CharField()
     slug = forms.SlugField()
