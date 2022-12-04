b56ee1604fa68c56f09c933cdef9f12f84177a5f
“sadikturan” <sadikturan@gmail.com>
2022-12-04 22:26:02 +0300

    284_7_12_form_templates

--- courses/forms.py
+++ courses/forms.py
@@ -1,9 +1,13 @@
 from django import forms
 
 class CourseCreateForm(forms.Form):
-    title = forms.CharField(label="kurs başlığı",required=True, error_messages={
-        "required":"kurs başlığı girmelisiniz."
-    })
-    description = forms.CharField(widget=forms.Textarea)
-    imageUrl = forms.CharField()
-    slug = forms.SlugField()
+    title = forms.CharField(
+        label="kurs başlığı",
+        required=True, 
+        error_messages= {
+            "required":"kurs başlığı girmelisiniz."}, 
+        widget=forms.TextInput(attrs={"class":"form-control"}))
+        
+    description = forms.CharField(widget=forms.Textarea(attrs={"class":"form-control"}))
+    imageUrl = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))
+    slug = forms.SlugField(widget=forms.TextInput(attrs={"class":"form-control"}))
